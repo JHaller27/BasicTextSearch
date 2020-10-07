3458b1e445d6c898ce8a5eb3148291c8da326abf
james <jameshaller27@gmail.com>
2020-10-07 11:30:21 -0700

    Update test to show off default search method

--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 from textSearch import *
 
 
-searcher = TextSearcher()
 method = PrioritySearch()
+searcher = TextSearcher(method)
 
 with open('data/grimms_tales.txt', 'r') as fin:
     text = fin.read()
     text = text.replace('\n', ' ')
     for sentence in text.split('.'):
-        searcher.add(sentence)
+        searcher.add(sentence + ".")
 
-# print(searcher)
+print(repr(searcher))
 
 terms = ["house", "the", "and", "good"]
-print("Searching with [", " + ".join(terms), "] and", str(method))
-found = searcher.search(terms, method)
+print("Searching with [", " + ".join(terms), "]")
+found = searcher.search(terms)
 
 count = 0
-for x in found:
+for x in reversed(list(found)):
     count += 1
 
     print(x)
